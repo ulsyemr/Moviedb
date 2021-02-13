3f166341d18f9a9b5dabe85249beaebbfff2af56
ulsyemr <ulsyemr@gmail.com>
2021-02-13 20:02:39 +0300

    all changes updated

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import axios from 'axios'
 
-import Search from './components/Search';
-import Results from './components/Results';
+import Search from './components/Search'
+import Results from './components/Results'
+import Popup from './components/Popup'
 
 function App() {
   const [state, setState] = useState({
@@ -10,39 +11,60 @@ function App() {
     results: [],
     selected: {}
   });
-  const apıurl = "http://www.omdbapi.com/?i=tt3896198&apikey=cb42259e";
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
 
   const search = (e) => {
-    if ( e.key === "Enter") {
-      axios(apıurl + "&s=" + state.s).then(( { data }) => {
-        let result = data.Search;
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
 
         setState(prevState => {
-          return {...prevState, result :result}
+          return { ...prevState, results: results }
         })
-      }); 
+      });
     }
   }
-
+  
   const handleInput = (e) => {
     let s = e.target.value;
-    
+
     setState(prevState => {
-      return {...prevState, s : s}
+      return { ...prevState, s : s }
     });
-    
   }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+
   return (
     <div className="App">
       <header>
-        <h1>Movie DB</h1>
+        <h1>Movie Database</h1>
       </header>
       <main>
-        <Search handleInput = {handleInput} search={search}></Search>
-        <Results results={state.results} />
+        <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
       </main>
     </div>
   );
 }
 
-export default App;
+export default App
--- moviedb/src/components/Result.js
+++ moviedb/src/components/Result.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Result( { result }) {
-  return (
-    <div className = "result">
-        <h3>{result.Title}</h3>
-        <img src={result.Poster} alt ="description of images" />
-    </div>
-  )
+function Result({ result, openPopup }) {
+	return (
+		<div className="result" onClick={() => openPopup(result.imdbID)}>
+			<img src={result.Poster} alt = "ads"/>
+			<h3>{result.Title}</h3>
+		</div>
+	)
 }
 
 export default Result
